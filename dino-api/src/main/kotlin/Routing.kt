dbb5887d29d746fd56ee49e246ece218d49fa378
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-01-20 20:23:15 +0800

    initial commit for dino-api

---
+++ dino-api/src/main/kotlin/Routing.kt
@@ -0,0 +1,13 @@
+package com.ralphmarondev
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello there, Ralph Maron Eda is here!")
+        }
+    }
+}
