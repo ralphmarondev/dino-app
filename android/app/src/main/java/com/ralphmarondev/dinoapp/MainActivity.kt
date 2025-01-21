ff7d3133ba1fc9957dd258a087305ff023cdfcff
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-01-21 18:45:38 +0800

    [android] updated UI and MainViewModel

--- android/app/src/main/java/com/ralphmarondev/dinoapp/MainActivity.kt
+++ android/app/src/main/java/com/ralphmarondev/dinoapp/MainActivity.kt
@@ -4,32 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Refresh
+import androidx.compose.material.icons.outlined.DarkMode
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
 import com.ralphmarondev.dinoapp.ui.theme.DinoAppTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -40,56 +55,109 @@ class MainActivity : ComponentActivity() {
             DinoAppTheme {
                 Scaffold(
                     topBar = {
-                        TopAppBar(
-                            title = {
-                                Text(
-                                    text = "Dino App",
-                                    fontFamily = FontFamily.Monospace
+                        HomeTopBar()
+                    },
+                    bottomBar = {
+                        NavigationBar(
+                            containerColor = Color.Transparent
+                        ) {
+                            ElevatedCard(
+                                onClick = viewModel::getRandomDino,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 16.dp, horizontal = 8.dp),
+                                colors = CardDefaults.elevatedCardColors(
+                                    containerColor = MaterialTheme.colorScheme.secondary,
+                                    contentColor = MaterialTheme.colorScheme.onSecondary
                                 )
-                            },
-                            actions = {
-                                IconButton(
-                                    onClick = viewModel::getRandomDino
+                            ) {
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(16.dp),
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center
                                 ) {
-                                    Icon(
-                                        imageVector = Icons.Outlined.Refresh,
-                                        contentDescription = "Refresh"
+                                    Text(
+                                        text = "Get Random Dinosaur",
+                                        fontFamily = FontFamily.Monospace,
+                                        textAlign = TextAlign.Center
                                     )
                                 }
-                            },
-                            colors = TopAppBarDefaults.topAppBarColors(
-                                containerColor = MaterialTheme.colorScheme.primary,
-                                titleContentColor = MaterialTheme.colorScheme.onPrimary,
-                                actionIconContentColor = MaterialTheme.colorScheme.onPrimary
-                            )
-                        )
+                            }
+                        }
                     }
                 ) { innerPadding ->
-                    Column(
+                    LazyColumn(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(innerPadding)
                     ) {
-                        Text(
-                            text = randomDino.name,
-                            fontFamily = FontFamily.Monospace,
-                            fontWeight = FontWeight.W500,
-                            fontSize = 20.sp,
-                            textAlign = TextAlign.Center,
-                            color = MaterialTheme.colorScheme.secondary,
-                            modifier = Modifier.padding(16.dp)
-                        )
-                        Text(
-                            text = randomDino.description,
-                            fontFamily = FontFamily.Monospace
-                        )
-                        Text(
-                            text = randomDino.imageUrl,
-                            fontFamily = FontFamily.Monospace
-                        )
+                        item {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(8.dp)
+                            ) {
+                                Image(
+                                    painter = rememberAsyncImagePainter(randomDino.imageUrl),
+                                    contentDescription = "Dinosaur Image",
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(240.dp)
+                                        .clip(RoundedCornerShape(8.dp)),
+                                    contentScale = ContentScale.Crop
+                                )
+                            }
+
+                            Text(
+                                text = randomDino.name,
+                                fontFamily = FontFamily.Monospace,
+                                fontWeight = FontWeight.W500,
+                                fontSize = 20.sp,
+                                color = MaterialTheme.colorScheme.primary,
+                                modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+                            )
+                            Text(
+                                text = randomDino.description,
+                                fontFamily = FontFamily.Monospace,
+                                fontWeight = FontWeight.W400,
+                                fontSize = 16.sp,
+                                color = MaterialTheme.colorScheme.secondary,
+                                modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+                            )
+                        }
                     }
                 }
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun HomeTopBar() {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Dino App",
+                fontFamily = FontFamily.Monospace
+            )
+        },
+        actions = {
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.DarkMode,
+                    contentDescription = "Theme Icon"
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimary
+        )
+    )
 }
--- android/app/src/main/java/com/ralphmarondev/dinoapp/MainViewModel.kt
+++ android/app/src/main/java/com/ralphmarondev/dinoapp/MainViewModel.kt
@@ -11,18 +11,14 @@ import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
     private val dinoApi = RetrofitInstance.dinoApi
-    private val _randomDino = MutableStateFlow<Dino>(
+    private val _randomDino = MutableStateFlow(
         Dino(
-            name = "",
-            description = "",
-            imageUrl = ""
+            name = "Tyrannosaurus",
+            description = "One of the most famous dinosaurs, Tyrannosaurus rex (T. rex) was a fearsome carnivore with powerful jaws and sharp teeth. It lived during the Late Cretaceous period and could grow up to 40 feet long. Its name means \"Tyrant Lizard.\"",
+            imageUrl = "https://scitechdaily.com/images/Tyrannosaurus-rex-Dinosaur.jpg"
         )
     )
     val randomDino: StateFlow<Dino> = _randomDino
-
-    init {
-        getRandomDino()
-    }
 
     fun getRandomDino() {
         try {
