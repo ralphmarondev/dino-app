6717def3c09aba017edb1ed72d5c30eb294aab2e
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-01-21 17:48:33 +0800

    [dino-api] updated Application.kt and application.yaml

--- dino-api/src/main/kotlin/Application.kt
+++ dino-api/src/main/kotlin/Application.kt
@@ -1,9 +1,14 @@
 package com.ralphmarondev
 
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+//    io.ktor.server.netty.EngineMain.main(args)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        module()
+    }.start(wait = true)
 }
 
 @Suppress("unused")
